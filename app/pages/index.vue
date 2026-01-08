2d660ba6f2db75e3f84afa0eb73f4894d0c31dd6
MuhamadAfghan <muhamadafghanalzena@gmail.com>
2026-01-09 01:27:14 +0700

    Hi

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,15 +1,3 @@
 <template>
-  <div class="space-y-10 pb-10">
-    <HomeHeroSlider />
-    <HomePromoSection />
-    <HomeGroupBuySection />
-    <HomeNewDropsSection />
-    <HomeTopSellerSection />
-    <HomeBrandsSection />
-    <HomeRecommendedSection />
-    <HomeTikTokSection />
-    <HomeLatestNewsSection />
-    <HomeWhyChooseSection />
-    <HomeCTASection />
-  </div>
+  <div class="space-y-10 pb-10">Hi</div>
 </template>
